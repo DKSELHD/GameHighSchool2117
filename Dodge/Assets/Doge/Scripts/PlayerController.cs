5e713dacd519fde2010128bb6cfca547675f89a4
Jinsanhyung <kygg9495@gmail.com>
2020-07-01 19:25:55 +0900

    닷지,돗지

---
+++ Dodge/Assets/Doge/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    public float m_Speed = 6f;
+    void Update()
+    {
+
+        Rigidbody rigidbody = /*gameObject.*/GetComponent<Rigidbody>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        
+
+        rigidbody.AddForce(new Vector3(xAxis, 0, yAxis) * m_Speed);
+
+
+        float fireAxis = Input.GetAxis("Fire1");
+
+        if (fireAxis > 0.95f)
+            Die();
+    }
+
+    public void Die()
+    {
+        Debug.Log("사망");
+        gameObject.SetActive(false);
+    }
+}
---
+++ Dodge/Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Test : EditorWindow
+{
+    [MenuItem("Window/Test")]
+
+    static void Init()
+    {
+        Test Window = (Test)EditorWindow.GetWindow(typeof(Test));
+        Window.Show();
+    }
+
+    void OnGUI()
+    {
+        Handles.color = Color.black;
+        Handles.DrawRectangle(0, new Vector3(200, 200, 0), Quaternion.identity, 100);
+        Handles.DrawSolidDisc(new Vector3(200, 200, 0), Vector3.forward, 100);
+        Handles.DrawSolidDisc(new Vector3(150, 150, 0), Vector3.forward, 50);
+        Handles.DrawSolidDisc(new Vector3(250, 150, 0), Vector3.forward, 50);
+    }
+}
