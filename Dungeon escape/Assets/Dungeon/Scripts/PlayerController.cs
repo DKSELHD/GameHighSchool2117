3195e5e3a0385f14c459a8749755b203f7256cdd
Jinsanhyung <kygg9495@gmail.com>
2020-07-07 21:19:22 +0900

    함정게임이다.

---
+++ Dungeon escape/Assets/Dungeon/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    public float m_Speed = 3f;
+    void Update()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(new Vector3(xAxis, 0, yAxis) * m_Speed);
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+        Debug.Log("사망");
+    }
+}
---
+++ Dungeon escape/Assets/Dungeon/Scripts/Trap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Vector3 m_Velocity;
+    // Update is called once per frame
+
+    
+    void Update()
+    {
+        
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody.tag == "Player")
+        {
+            PlayerController player = other.attachedRigidbody.GetComponent<PlayerController>();
+            player.Die();
+        }
+    }
+}
