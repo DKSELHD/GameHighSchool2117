68d1dd5461a2dadaa4361fa75ed275bec336b8c2
Jinsanhyung <kygg9495@gmail.com>
2020-07-08 21:13:40 +0900

    던전 던전 던전

---
+++ Dodge/Assets/Dungeon/Scenes/Scrpits/Playerolle_dneon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerolle_dneon : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+
+    public Rigidbody m_Rigidbody;
+
+    public float m_Speed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+    
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+
+        Vector3 velocity = new Vector3(xAxis, 0, zAxis).normalized * m_Speed;
+        Vector3 movement = velocity * Time.deltaTime;
+        transform.position = transform.position + movement;
+
+    
+        m_Rigidbody.velocity = velocity;
+
+    }
+
+    public void Die()
+    {
+        Debug.Log("사망");
+        gameObject.SetActive(false);
+
+
+
+    }
+
+}
---
+++ Dodge/Assets/Dungeon/Scenes/Scrpits/Trap2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap2 : MonoBehaviour
+{
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Collision에 어떤 collision과 충돌이 끝났을 때");
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision에 어떤 collisio과 충돌이 일어나고 있을 때");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.rigidbody != null)
+        {
+            var player = collision.rigidbody
+                .GetComponent<Playerolle_dneon>();
+            if (player != null)
+                player.Die();
+        }
+
+        Debug.Log("Collision이 어떤 collision과 충돌이 일어 났을 때");
+    }
+}
