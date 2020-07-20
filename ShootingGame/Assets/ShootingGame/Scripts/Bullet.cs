91dddd243ef826191a1da6d0fd9047016e5623dd
Jinsanhyung <kygg9495@gmail.com>
2020-07-20 21:11:04 +0900

    우주, 돗지, 엿지, 갓지, 우주, 우주

---
+++ ShootingGame/Assets/ShootingGame/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    
+
+    public float m_Speed = 2f;
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        //transform.position += transform.up * m_Speed * Time.deltaTime;
+
+        Vector3 movement = transform.up * m_Speed * Time.deltaTime;
+       // Vector3 velocity = velocity * Time.deltaTime;
+        
+
+        transform.position += movement;
+
+
+    }
+}
---
+++ ShootingGame/Assets/ShootingGame/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float m_Speed = 10f;
+    // Update is called once per frame
+
+    private void Update()
+    {
+
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector2 inputValue = new Vector2(xAxis, yAxis);
+
+
+        //if (Input.GetKeyDown(KeyCode.LeftArrow))
+        //{
+        //    inputValue.x = -1;
+        //}
+        //if (Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+        //    inputValue.x = 1;
+        //}
+        //if (Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    inputValue.y = - 1;
+        //}
+        //if (Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        //    inputValue.y = 1;
+        //}
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+
+        //}
+
+        Vector3 movement = inputValue * m_Speed * Time.deltaTime;
+        transform.position += movement;
+    }
+    //void Update()
+    //{
+
+
+
+    //    Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+    //    float yAxis = Input.GetAxis("Vertical");
+
+    //    rigidbody.AddForce(new Vector3(yAxis, 0) * m_Speed);
+    //}
+}
