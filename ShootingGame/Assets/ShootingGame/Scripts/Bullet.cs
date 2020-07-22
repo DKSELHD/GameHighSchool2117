49c58ef1ca71edff7ab07bd0ff122e54b1859384
Jinsanhyung <kygg9495@gmail.com>
2020-07-22 21:01:05 +0900

    비행비행

--- ShootingGame/Assets/ShootingGame/Scripts/Bullet.cs
+++ ShootingGame/Assets/ShootingGame/Scripts/Bullet.cs
@@ -4,21 +4,12 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    
-
     public float m_Speed = 2f;
-    
-    // Update is called once per frame
+
     private void Update()
     {
-        //transform.position += transform.up * m_Speed * Time.deltaTime;
-
-        Vector3 movement = transform.up * m_Speed * Time.deltaTime;
-       // Vector3 velocity = velocity * Time.deltaTime;
-        
-
+        Vector3 velocity = transform.up * m_Speed;
+        Vector3 movement = velocity * Time.deltaTime;
         transform.position += movement;
-
-
     }
 }
