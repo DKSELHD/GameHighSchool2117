4c2d39791625712b33cebc19cbeed382d802bba4
Jinsanhyung <kygg9495@gmail.com>
2020-09-02 21:15:29 +0900

    서니 랜드

--- SunnyLand/Assets/GameSchool/Scripts/PlayController.cs
+++ SunnyLand/Assets/GameSchool/Scripts/PlayController.cs
@@ -18,6 +18,12 @@ public class PlayController : MonoBehaviour
     public Animator m_Animator;
     public bool m_IsJumping = false;
 
+    public bool m_IsClimbing = false;
+
+    public bool m_IsTouchLadder = false;
+
+    public float m_ClimbSpeed = 5f;
+
 
     protected void Start()
     {
@@ -30,34 +36,74 @@ public class PlayController : MonoBehaviour
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
 
-        Vector2 velocity = m_Rigidbody2D.velocity;
-        velocity.x = xAxis * m_xAxisSpeed;
-        m_Rigidbody2D.velocity = velocity;
-
-        if (xAxis > 0)
-            m_Sprite.localScale = new Vector3(1, 1, -1);
-        else if (xAxis < 0)
-            m_Sprite.localScale = new Vector3(-1, 1, 1);
-
-        if (Input.GetKeyDown(KeyCode.UpArrow)
-            && m_JumpCount <= 0)
+        if(m_IsTouchLadder && Mathf.Abs(yAxis) > 0.5f)
         {
-            m_Rigidbody2D.AddForce(Vector3.up * m_YJumpPower);
-
-            m_JumpCount++;
+            m_IsClimbing = true;
         }
 
-        m_IsJumping = Mathf.Abs(velocity.y) >= 0.1f ? true : false;
+        if(!m_IsClimbing)
+        {
+            Vector2 velocity = m_Rigidbody2D.velocity;
+            velocity.x = xAxis * m_xAxisSpeed;
+            m_Rigidbody2D.velocity = velocity;
 
-        m_Animator.SetBool("isJumping", m_IsJumping);
-        m_Animator.SetFloat("velocity X", Mathf.Abs(xAxis));
-        //var animator = GetComponent<Animator>();
-        //animator.SetFloat("velocity Y", velocity.y);
+            if (xAxis > 0)
+                m_Sprite.localScale = new Vector3(1, 1, -1);
+            else if (xAxis < 0)
+                m_Sprite.localScale = new Vector3(-1, 1, 1);
 
-        //점프 애니메이션
-        m_Animator.SetFloat("velocity Y", velocity.y);
+            if (Input.GetKeyDown(KeyCode.UpArrow)
+                && m_JumpCount <= 0)
+            {
+                m_Rigidbody2D.AddForce(Vector3.up * m_YJumpPower);
 
-       
+                m_JumpCount++;
+            }
+
+            m_IsJumping = Mathf.Abs(velocity.y) >= 0.1f ? true : false;
+
+            m_Animator.SetBool("isJumping", m_IsJumping);
+            m_Animator.SetFloat("velocity X", Mathf.Abs(xAxis));
+            //var animator = GetComponent<Animator>();
+            //animator.SetFloat("velocity Y", velocity.y);
+
+            //점프 애니메이션
+            m_Animator.SetFloat("velocity Y", velocity.y);
+
+           
+        }
+        else
+        {
+
+            m_Rigidbody2D.constraints = 
+                m_Rigidbody2D.constraints 
+                | RigidbodyConstraints2D.FreezePosition;
+
+
+            Vector3 movement = Vector3.zero;
+            movement.x = xAxis * m_ClimbSpeed * Time.deltaTime;
+            movement.y = yAxis * m_ClimbSpeed * Time.deltaTime;
+
+            transform.position += movement;
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                ClimbingExit();
+            }
+
+            m_Animator.SetBool("IsClibing", m_IsClimbing);
+            m_Animator.SetFloat("ClibingSpeed",
+                Mathf.Abs(xAxis) + Mathf.Abs(yAxis));
+            
+        }
+        
+    }
+
+    private void ClimbingExit()
+    {
+        m_Rigidbody2D.constraints =
+            m_Rigidbody2D.constraints & ~RigidbodyConstraints2D.FreezePosition;
+        m_IsClimbing = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -69,6 +115,26 @@ public class PlayController : MonoBehaviour
             {
                 m_JumpCount = 0;
             }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ladder")
+        {
+            m_IsTouchLadder = true;
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Ladder")
+        {
+            m_IsTouchLadder = false;
+
+            ClimbingExit();
         }
     }
 
