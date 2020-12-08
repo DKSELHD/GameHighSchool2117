fc85f02610e149e3d606dce9e06aba4359102a68
Jinsanhyung <kygg9495@gmail.com>
2020-12-08 20:59:01 +0900

    Photon

---
+++ PhotonAPI/Assets/Photon/Scripts/MyPhotonNetwork.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MyPhotonNetwork : MonoBehaviourPunCallbacks
+{
+    public override void OnConnected()
+    {
+        Debug.Log("OnConnected");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("OnDisConnected");
+    }
+
+    public void OnClickConnect()
+    {
+        Photon.Pun.PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public void OnClickDisonnect()
+    {
+        Photon.Pun.PhotonNetwork.Disconnect();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        if(PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        PhotonNetwork.LoadLevel("Level_Test1");
+    }
+   
+}
